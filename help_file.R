d5d81f605eb743ca9e4995ea44444a31e16d09fd
MislavSag <mislav.sagovac@contentio.biz>
2020-04-10 23:06:00 +0200

    change help file

--- help_file.R
+++ help_file.R
@@ -1,18 +1,17 @@
 # generate fodlers for stire
 rmarkdown::render_site()
 
-# delete 
-
 ## help code to move public file to Cpanel
 if (file.exists('_site.zip')) {
   file.remove('_site.zip')
 }
 files2zip <- dir('_site', full.names = TRUE)
+files2zip <- setdiff(files2zip, '_site/data_temp')
 zip(zipfile = "_site", files = files2zip)
 
 # send public.zip to server
 library(RCurl)
-ftpUpload("C:/Users/Mislav/covidrh/_site.zip", 
+ftpUpload("C:/Users/Mislav/rhdash/_site.zip", 
           "ftp://ftp.odvjetnistvo-sagovac.hr/_site.zip", 
           userpwd = "rh-dash@rh-dash.contentio.biz:Theanswer0207")
 
@@ -24,7 +23,7 @@ m <- machine(hostname = "91.234.46.219",
              priv.key.ppk = 'D:/data/R/CroEconSite/id_rsa.ppk')
 # .pbd_env$RPC.CT$use.shell.exec <- FALSE
 rpc(
-  m, 
+  m,
   'cd public_html; rm -rf rh-dash.contentio.biz; unzip _site.zip; mv _site rh-dash.contentio.biz; rm -rf _site.zip'
 )
 
